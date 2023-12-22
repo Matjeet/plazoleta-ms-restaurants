4e772ef73f34160b338665aad864be8d153f2793
Mateo Velásquez Morales <matius29002@gmail.com>
2023-12-22 13:34:38 -0500

    feat: created new IRestaurantServicePort interface within domain/api

---
+++ src/main/java/com/pragma/powerup/domain/api/IRestaurantServicePort.java
@@ -0,0 +1,8 @@
+package com.pragma.powerup.domain.api;
+
+import com.pragma.powerup.domain.model.Restaurant;
+
+public interface IRestaurantServicePort {
+
+    void saveRestaurant(Restaurant restaurant);
+}
