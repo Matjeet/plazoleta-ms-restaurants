f305161fdbec42a8fcc1657669e588c6e98fc77a
Mateo Velásquez Morales <matius29002@gmail.com>
2023-12-22 14:00:25 -0500

    feat: created new RegisterRestaurantDto within application/dto/request

---
+++ src/main/java/com/pragma/powerup/application/dto/request/RegisterRestaurantDto.java
@@ -0,0 +1,16 @@
+package com.pragma.powerup.application.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRestaurantDto {
+
+    private String name;
+    private String nit;
+    private String address;
+    private String phone;
+    private String logoUrl;
+    private int idOwner;
+}
