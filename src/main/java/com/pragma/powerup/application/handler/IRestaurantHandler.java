c32cca6ab4e7f5c7c00ba53b3e3d2e7f880c3267
Mateo Velásquez Morales <matius29002@gmail.com>
2023-12-22 14:08:33 -0500

    feat: created new IRestaurantHandler interface and RestaurantHandler class

---
+++ src/main/java/com/pragma/powerup/application/handler/IRestaurantHandler.java
@@ -0,0 +1,8 @@
+package com.pragma.powerup.application.handler;
+
+import com.pragma.powerup.application.dto.request.RegisterRestaurantDto;
+
+public interface IRestaurantHandler {
+
+    void saveRestaurant(RegisterRestaurantDto registerRestaurantDto);
+}
---
+++ src/main/java/com/pragma/powerup/application/handler/impl/RestaurantHandler.java
@@ -0,0 +1,11 @@
+package com.pragma.powerup.application.handler.impl;
+
+import com.pragma.powerup.application.dto.request.RegisterRestaurantDto;
+import com.pragma.powerup.application.handler.IRestaurantHandler;
+
+public class RestaurantHandler implements IRestaurantHandler {
+    @Override
+    public void saveRestaurant(RegisterRestaurantDto registerRestaurantDto) {
+        
+    }
+}
