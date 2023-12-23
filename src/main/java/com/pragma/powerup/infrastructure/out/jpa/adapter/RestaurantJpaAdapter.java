9932e06d739547cc0c074f18a6ec0397c98e510d
Mateo Velásquez Morales <matius29002@gmail.com>
2023-12-22 21:58:09 -0500

    feat: created new RestaurantJpaAdapter class within infrastructure/out/adapter

---
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/RestaurantJpaAdapter.java
@@ -0,0 +1,25 @@
+package com.pragma.powerup.infrastructure.out.jpa.adapter;
+
+import com.pragma.powerup.domain.model.Restaurant;
+import com.pragma.powerup.domain.spi.IRestaurantPersistencePort;
+import com.pragma.powerup.infrastructure.out.jpa.mapper.IRestaurantEntityMapper;
+import com.pragma.powerup.infrastructure.out.jpa.repository.IRestaurantRespository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class RestaurantJpaAdapter implements IRestaurantPersistencePort {
+
+    private final IRestaurantRespository restaurantRespository;
+    private final IRestaurantEntityMapper restaurantEntityMapper;
+
+    @Override
+    public boolean saveRestaurant(Restaurant restaurant) {
+        try{
+            restaurantRespository.save(restaurantEntityMapper.toEntity(restaurant));
+        }
+        catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+}
