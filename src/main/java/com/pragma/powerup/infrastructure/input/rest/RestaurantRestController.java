24c7756aaa2610b61cc26416badf74d0eb7fcd64
Mateo Velásquez Morales <matius29002@gmail.com>
2023-12-22 21:40:04 -0500

    feat: created new RestaurantRestController into infrastucture/input/rest

---
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/RestaurantRestController.java
@@ -0,0 +1,31 @@
+package com.pragma.powerup.infrastructure.input.rest;
+
+import com.pragma.powerup.application.dto.request.RegisterRestaurantDto;
+import com.pragma.powerup.application.handler.IRestaurantHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("restaurant/v1/")
+@RequiredArgsConstructor
+public class RestaurantRestController {
+
+    private final IRestaurantHandler restaurantHandler;
+
+    @PostMapping("/create")
+    public ResponseEntity<Void> saveRestaurant(@RequestBody RegisterRestaurantDto registerRestaurantDto){
+        if(restaurantHandler.saveRestaurant(registerRestaurantDto)){
+            return ResponseEntity
+                    .status(HttpStatus.CREATED)
+                    .build();
+        }
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .build();
+    }
+}
