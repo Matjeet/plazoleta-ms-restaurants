5655c87e03f46d31a1291e356aca195d7974154c
Mateo Velásquez Morales <matius29002@gmail.com>
2023-12-28 17:24:03 -0500

    feat: added new Security dependency and configuration

---
+++ src/main/java/com/pragma/powerup/infrastructure/configuration/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.pragma.powerup.infrastructure.configuration;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
+public class WebSecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        return http
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/swagger-ui/**", "/v3/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .build();
+    }
+}
