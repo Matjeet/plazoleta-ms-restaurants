9306157d4cce38d58c6a09b483266c3f84064222
Mateo Velásquez Morales <matius29002@gmail.com>
2023-12-28 15:53:05 -0500

    feat: microservice-users can now consume the validateOwnerRole endpoint of the microservice-locals

--- src/main/java/com/pragma/powerup/application/handler/impl/RestaurantHandler.java
+++ src/main/java/com/pragma/powerup/application/handler/impl/RestaurantHandler.java
@@ -20,7 +20,7 @@ public class RestaurantHandler implements IRestaurantHandler {
     private final IOwnerFeignClient ownerFeignClient;
     @Override
     public boolean saveRestaurant(RegisterRestaurantDto registerRestaurantDto) {
-        if(ownerFeignClient.validateOwnerRole(registerRestaurantDto.getIdOwner())){
+        if(ownerFeignClient.validateOwnerRole("Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJtYXRpdXMyOTA1M0BnbWFpbC5jb20iLCJleHAiOjE3MDM4MDU0NDEsInJvbGUiOlsiUk9MRV9hZG1pbmlzdHJhZG9yIl0sIm5hbWUiOiJtYXRpdXMyOTA1M0BnbWFpbC5jb20ifQ.HlJo1i7jLwPqm3l0wZ9ImRQHgturE9khTTDbQoqkiGA",registerRestaurantDto.getIdOwner())){
             Restaurant restaurant = restaurantRequestMapper.toRestaurant(registerRestaurantDto);
             return restaurantPersistencePort.saveRestaurant(restaurant);
         }
