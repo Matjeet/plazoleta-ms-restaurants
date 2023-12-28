f53398e715d5f950e9bf72473ea58a9c95a24c15
Mateo Velásquez Morales <matius29002@gmail.com>
2023-12-28 18:01:45 -0500

    feat: added filter inside the WebSecurityConfig class and annotation preauthorize above saveRestaurant method inside the RestaurantRestController

--- src/main/java/com/pragma/powerup/infrastructure/configuration/WebSecurityConfig.java
+++ src/main/java/com/pragma/powerup/infrastructure/configuration/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.pragma.powerup.infrastructure.configuration;
 
+import com.pragma.powerup.infrastructure.token.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,12 +9,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
@@ -26,6 +30,7 @@ public class WebSecurityConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 }
--- src/main/java/com/pragma/powerup/infrastructure/input/rest/RestaurantRestController.java
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/RestaurantRestController.java
@@ -5,6 +5,7 @@ import com.pragma.powerup.application.handler.IRestaurantHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class RestaurantRestController {
     private final IRestaurantHandler restaurantHandler;
 
     @PostMapping("/create")
+    @PreAuthorize("hasRole('ROLE_administrador')")
     public ResponseEntity<Void> saveRestaurant(@RequestBody RegisterRestaurantDto registerRestaurantDto){
         if(restaurantHandler.saveRestaurant(registerRestaurantDto)){
             return ResponseEntity
