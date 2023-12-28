b084907164a48214e67dad8e7830d02d22651042
Mateo Velásquez Morales <matius29002@gmail.com>
2023-12-28 17:31:25 -0500

    feat: added new JwtAuthenticationFilter class within infrastructure

---
+++ src/main/java/com/pragma/powerup/infrastructure/exception/TokenRequiredException.java
@@ -0,0 +1,8 @@
+package com.pragma.powerup.infrastructure.exception;
+
+public class TokenRequiredException extends RuntimeException{
+
+    public  TokenRequiredException() {
+        super();
+    }
+}
