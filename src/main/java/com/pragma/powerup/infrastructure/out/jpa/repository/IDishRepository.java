33a0b9af40dbf80d610e5a08fcb6f02b6ed518bf
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-02 18:00:11 -0500

    feat: added new method to find a dish inside the IDishRepository

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/repository/IDishRepository.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/repository/IDishRepository.java
@@ -3,5 +3,9 @@ package com.pragma.powerup.infrastructure.out.jpa.repository;
 import com.pragma.powerup.infrastructure.out.jpa.entity.DishEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface IDishRepository extends JpaRepository<DishEntity, Integer> {
+
+    Optional<DishEntity> findByNameAndPrice(String name, int price);
 }
