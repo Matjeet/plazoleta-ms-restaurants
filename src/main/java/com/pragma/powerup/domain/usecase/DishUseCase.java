140c37cf49c2a1bcfc956d91f5c556abf4520fda
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-02 16:09:51 -0500

    feat: new DishUseCase class and InvalidPriceException class within domain

---
+++ src/main/java/com/pragma/powerup/domain/usecase/DishUseCase.java
@@ -0,0 +1,25 @@
+package com.pragma.powerup.domain.usecase;
+
+import com.pragma.powerup.domain.api.IDishServicePort;
+import com.pragma.powerup.domain.exception.InvalidPriceException;
+import com.pragma.powerup.domain.model.Dish;
+import com.pragma.powerup.domain.spi.IDishPersistencePort;
+
+public class DishUseCase implements IDishServicePort {
+
+    private final IDishPersistencePort dishPersistencePort;
+    public DishUseCase(IDishPersistencePort dishPersistencePort) {
+        this.dishPersistencePort = dishPersistencePort;
+    }
+    @Override
+    public void saveDish(Dish dish) {
+
+        if(dish.getPrice() > 0) {
+            dishPersistencePort.saveDish(dish);
+        }
+        else {
+            throw new InvalidPriceException();
+        }
+
+    }
+}
