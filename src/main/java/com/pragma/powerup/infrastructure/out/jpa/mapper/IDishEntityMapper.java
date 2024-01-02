cce3ddf74a0e63f6c87a70979966855511cda710
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-02 17:53:48 -0500

    feat: new IDishEntityMapper interface within infrastructure

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/mapper/IDishEntityMapper.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/mapper/IDishEntityMapper.java
@@ -1,2 +1,18 @@
-package com.pragma.powerup.infrastructure.out.jpa.mapper;public interface IDishEntityMapper {
+package com.pragma.powerup.infrastructure.out.jpa.mapper;
+
+import com.pragma.powerup.domain.model.Dish;
+import com.pragma.powerup.infrastructure.out.jpa.entity.CategoryEntity;
+import com.pragma.powerup.infrastructure.out.jpa.entity.DishEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.ERROR,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface IDishEntityMapper {
+
+    @Mapping(source = "dish.id", target = "id")
+    @Mapping(source = "dish.name", target = "name")
+    DishEntity toEntity(Dish dish, CategoryEntity category);
 }
