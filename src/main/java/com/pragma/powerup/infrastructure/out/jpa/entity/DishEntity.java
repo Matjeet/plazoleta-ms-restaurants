89c6e07df880a205b3db38b61f3a338ac6aa5f10
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-02 17:03:12 -0500

    feat: new DishEntity class within infrastructure

---
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/DishEntity.java
@@ -0,0 +1,30 @@
+package com.pragma.powerup.infrastructure.out.jpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dish")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class DishEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int price;
+    private String description;
+    private String urlImage;
+    private int idRestaurant;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private CategoryEntity Category;
+}
