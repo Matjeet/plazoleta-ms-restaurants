a4dbd615fdcdc7e15eb9f1914d57691301b70053
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-02 18:10:09 -0500

    feat: new DishJpaAdapter class and DishAlreadyExistException class

---
+++ src/main/java/com/pragma/powerup/infrastructure/exception/DishAlreadyExistException.java
@@ -0,0 +1,7 @@
+package com.pragma.powerup.infrastructure.exception;
+
+public class DishAlreadyExistException extends RuntimeException{
+    public DishAlreadyExistException(){
+        super();
+    }
+}
--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/DishJpaAdapter.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/DishJpaAdapter.java
@@ -1,2 +1,28 @@
-package com.pragma.powerup.infrastructure.out.jpa.adapter;public class DishJpaAdapter {
+package com.pragma.powerup.infrastructure.out.jpa.adapter;
+
+import com.pragma.powerup.domain.model.Dish;
+import com.pragma.powerup.domain.spi.IDishPersistencePort;
+import com.pragma.powerup.infrastructure.exception.DishAlreadyExistException;
+import com.pragma.powerup.infrastructure.out.jpa.entity.CategoryEntity;
+import com.pragma.powerup.infrastructure.out.jpa.mapper.IDishEntityMapper;
+import com.pragma.powerup.infrastructure.out.jpa.repository.IDishRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class DishJpaAdapter implements IDishPersistencePort {
+
+    private final IDishRepository dishRepository;
+    private final IDishEntityMapper dishEntityMapper;
+    @Override
+    public void saveDish(Dish dish) {
+
+        if (dishRepository.findByNameAndPrice(dish.getName(), dish.getPrice()).isPresent()) {
+            throw new DishAlreadyExistException();
+        }
+        else {
+            dishRepository.save(
+                    dishEntityMapper.toEntity(dish, new CategoryEntity())
+            );
+        }
+    }
 }
