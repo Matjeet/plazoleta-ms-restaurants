97d3f702d146efa6641a24c85cbbeabd4575ff49
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-02 18:35:47 -0500

    feat: new CategoryJpaAdapter class within infrastructure

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/CategoryJpaAdapter.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/CategoryJpaAdapter.java
@@ -1,2 +1,25 @@
-package com.pragma.powerup.infrastructure.out.jpa.adapter;public class CategoryJpaAdapter {
+package com.pragma.powerup.infrastructure.out.jpa.adapter;
+
+import com.pragma.powerup.domain.model.Category;
+import com.pragma.powerup.domain.spi.ICategoryPersistencePort;
+import com.pragma.powerup.infrastructure.out.jpa.mapper.ICategoryEntityMapper;
+import com.pragma.powerup.infrastructure.out.jpa.repository.ICategoryRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CategoryJpaAdapter implements ICategoryPersistencePort {
+
+    private final ICategoryRepository categoryRepository;
+    private final ICategoryEntityMapper categoryEntityMapper;
+    @Override
+    public Category saveCategory(Category category) {
+        if(categoryRepository.findByName(category.getName()).isPresent()){
+            return categoryEntityMapper.toCategory(categoryRepository.getByName(category.getName()));
+        }
+        return categoryEntityMapper.toCategory(
+                categoryRepository.save(
+                        categoryEntityMapper.toEntity(category)
+                )
+        );
+    }
 }
