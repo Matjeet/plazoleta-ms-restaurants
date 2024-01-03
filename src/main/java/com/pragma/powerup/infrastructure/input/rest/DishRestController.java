7712ed939d30ae1638f86a71fad95db6e45803c8
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-03 16:16:22 -0500

    feat: added new documentation for the endpoint createDish in the DishRestController class and changed the url for the documentation inside the README

--- src/main/java/com/pragma/powerup/infrastructure/input/rest/DishRestController.java
+++ src/main/java/com/pragma/powerup/infrastructure/input/rest/DishRestController.java
@@ -2,6 +2,10 @@ package com.pragma.powerup.infrastructure.input.rest;
 
 import com.pragma.powerup.application.dto.request.RegisterDishDto;
 import com.pragma.powerup.application.handler.IDishHandler;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +22,16 @@ public class DishRestController {
 
     private final IDishHandler dishHandler;
 
+    @Operation(summary = "Create a restaurant menu dish")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "The dish was created successful",
+                    content = @Content),
+            @ApiResponse(responseCode = "403",
+                    description = "Only the restaurant owner can create a dish",
+                    content = @Content)
+    })
     @PreAuthorize("hasRole('ROLE_propietario')")
     @PostMapping("/create")
     public ResponseEntity<Void> createDish(@RequestBody RegisterDishDto registerDishDto){
