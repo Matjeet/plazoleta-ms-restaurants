6e6ac334a812e3e6d0b923a84774e2ff3e86c5bc
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-05 16:05:28 -0500

    feat: new StatusJpaAdapter class within infrastructure

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/StatusJpaAdapter.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/StatusJpaAdapter.java
@@ -1,2 +1,17 @@
-package com.pragma.powerup.infrastructure.out.jpa.adapter;public class StatusJpaAdapter {
+package com.pragma.powerup.infrastructure.out.jpa.adapter;
+
+import com.pragma.powerup.domain.spi.IStatusPersistencePort;
+import com.pragma.powerup.infrastructure.out.jpa.entity.StatusEntity;
+import com.pragma.powerup.infrastructure.out.jpa.repository.IStatusRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class StatusJpaAdapter implements IStatusPersistencePort {
+
+    private final IStatusRepository statusRepository;
+    @Override
+    public int getStatusId(String name) {
+        StatusEntity statusEntity = statusRepository.findByName(name);
+        return statusEntity.getId();
+    }
 }
