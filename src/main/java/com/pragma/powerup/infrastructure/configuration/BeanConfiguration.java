aa0c1329ac4750fbc07e8d4db190c394cedd75e2
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-05 16:08:31 -0500

    feat: added new configuration into the BeanConfiguration class

--- src/main/java/com/pragma/powerup/infrastructure/configuration/BeanConfiguration.java
+++ src/main/java/com/pragma/powerup/infrastructure/configuration/BeanConfiguration.java
@@ -1,29 +1,14 @@
 package com.pragma.powerup.infrastructure.configuration;
 
-import com.pragma.powerup.domain.api.ICategoryServicePort;
-import com.pragma.powerup.domain.api.IDishServicePort;
-import com.pragma.powerup.domain.api.IObjectServicePort;
-import com.pragma.powerup.domain.api.IRestaurantServicePort;
-import com.pragma.powerup.domain.spi.ICategoryPersistencePort;
-import com.pragma.powerup.domain.spi.IDishPersistencePort;
-import com.pragma.powerup.domain.spi.IObjectPersistencePort;
-import com.pragma.powerup.domain.spi.IRestaurantPersistencePort;
-import com.pragma.powerup.domain.usecase.CategoryUseCase;
-import com.pragma.powerup.domain.usecase.DishUseCase;
-import com.pragma.powerup.domain.usecase.ObjectUseCase;
-import com.pragma.powerup.domain.usecase.RestaurantUseCase;
-import com.pragma.powerup.infrastructure.out.jpa.adapter.CategoryJpaAdapter;
-import com.pragma.powerup.infrastructure.out.jpa.adapter.DishJpaAdapter;
-import com.pragma.powerup.infrastructure.out.jpa.adapter.ObjectJpaAdapter;
-import com.pragma.powerup.infrastructure.out.jpa.adapter.RestaurantJpaAdapter;
+import com.pragma.powerup.domain.api.*;
+import com.pragma.powerup.domain.spi.*;
+import com.pragma.powerup.domain.usecase.*;
+import com.pragma.powerup.infrastructure.out.jpa.adapter.*;
 import com.pragma.powerup.infrastructure.out.jpa.mapper.ICategoryEntityMapper;
 import com.pragma.powerup.infrastructure.out.jpa.mapper.IDishEntityMapper;
 import com.pragma.powerup.infrastructure.out.jpa.mapper.IObjectEntityMapper;
 import com.pragma.powerup.infrastructure.out.jpa.mapper.IRestaurantEntityMapper;
-import com.pragma.powerup.infrastructure.out.jpa.repository.ICategoryRepository;
-import com.pragma.powerup.infrastructure.out.jpa.repository.IDishRepository;
-import com.pragma.powerup.infrastructure.out.jpa.repository.IObjectRepository;
-import com.pragma.powerup.infrastructure.out.jpa.repository.IRestaurantRepository;
+import com.pragma.powerup.infrastructure.out.jpa.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -39,7 +24,17 @@ public class BeanConfiguration {
     private final IDishEntityMapper dishEntityMapper;
     private final ICategoryRepository categoryRepository;
     private final ICategoryEntityMapper categoryEntityMapper;
+    private final IStatusRepository statusRepository;
 
+    @Bean
+    public IStatusPersistencePort statusPersistencePort(){
+        return new StatusJpaAdapter(statusRepository);
+    }
+
+    @Bean
+    public IStatusServicePort statusServicePort(){
+        return new StatusUseCase(statusPersistencePort());
+    }
 
     @Bean
     public ICategoryPersistencePort categoryPersistencePort(){
