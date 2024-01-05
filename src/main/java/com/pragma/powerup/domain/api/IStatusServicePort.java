b2ad0ad85a7752d3795a69f2d86c1695d8a45acf
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-05 15:39:36 -0500

    feat: new IStatusServicePort interface and StatusUseCase class within domain

--- src/main/java/com/pragma/powerup/domain/api/IStatusServicePort.java
+++ src/main/java/com/pragma/powerup/domain/api/IStatusServicePort.java
@@ -1,2 +1,8 @@
-package com.pragma.powerup.domain.api;public interface IStatusServicePort {
+package com.pragma.powerup.domain.api;
+
+import com.pragma.powerup.domain.model.Status;
+
+public interface IStatusServicePort {
+
+    int getStatusId (String statusName);
 }
--- src/main/java/com/pragma/powerup/domain/usecase/StatusUseCase.java
+++ src/main/java/com/pragma/powerup/domain/usecase/StatusUseCase.java
@@ -1,2 +1,18 @@
-package com.pragma.powerup.domain.usecase;public class StatusUseCase {
+package com.pragma.powerup.domain.usecase;
+
+import com.pragma.powerup.domain.api.IStatusServicePort;
+import com.pragma.powerup.domain.model.Status;
+import com.pragma.powerup.domain.spi.IStatusPersistencePort;
+
+public class StatusUseCase implements IStatusServicePort {
+
+    private final IStatusPersistencePort statusPersistencePort;
+
+    public StatusUseCase(IStatusPersistencePort statusPersistencePort){
+        this.statusPersistencePort = statusPersistencePort;
+    }
+    @Override
+    public int getStatusId(String statusName) {
+        return statusPersistencePort.getStatusId(statusName);
+    }
 }
