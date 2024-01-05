6dde3043d9cbc6943fbca7e89e879586369256e3
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-05 16:03:56 -0500

    feat: added new method to find a Status entity by name

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/repository/IStatusRepository.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/repository/IStatusRepository.java
@@ -4,4 +4,6 @@ import com.pragma.powerup.infrastructure.out.jpa.entity.StatusEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IStatusRepository extends JpaRepository<StatusEntity, Integer> {
+
+    StatusEntity findByName(String statusName);
 }
