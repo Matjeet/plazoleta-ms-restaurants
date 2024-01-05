b7ba3f2fabc3e925acdce76be7690ec83734eca5
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-05 15:53:24 -0500

    feat: new StatusEntity class within infrastructure

---
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/StatusEntity.java
@@ -0,0 +1,22 @@
+package com.pragma.powerup.infrastructure.out.jpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "status")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class StatusEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+}
