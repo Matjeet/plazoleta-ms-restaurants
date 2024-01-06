311d77fa65e45dfd77717dc15e7764a1b0c40631
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-06 17:52:10 -0500

    feat: added new constant inside the Constants class

--- src/main/java/com/pragma/powerup/domain/Constants.java
+++ src/main/java/com/pragma/powerup/domain/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
 
     public static final String ROLE = "role";
     public static final String ENABLE = "habilitado";
+    public static final String DISABLE = "deshabilitado";
 }
