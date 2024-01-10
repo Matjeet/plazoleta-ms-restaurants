5f9ba6b1defcf87e0962c206cb03b015e309b702
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-10 12:28:59 -0500

    feat: added new getRestaurants method inside the IRestaurantPersistencePort and RestaurantJpaAdapter

--- src/main/java/com/pragma/powerup/domain/spi/IRestaurantPersistencePort.java
+++ src/main/java/com/pragma/powerup/domain/spi/IRestaurantPersistencePort.java
@@ -1,8 +1,14 @@
 package com.pragma.powerup.domain.spi;
 
 import com.pragma.powerup.domain.model.Restaurant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 public interface IRestaurantPersistencePort {
 
     boolean saveRestaurant(Restaurant restaurant);
+
+    Page<Restaurant> getRestaurants(Pageable pageable);
 }
--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/RestaurantJpaAdapter.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/RestaurantJpaAdapter.java
@@ -2,9 +2,15 @@ package com.pragma.powerup.infrastructure.out.jpa.adapter;
 
 import com.pragma.powerup.domain.model.Restaurant;
 import com.pragma.powerup.domain.spi.IRestaurantPersistencePort;
+import com.pragma.powerup.infrastructure.out.jpa.entity.RestaurantEntity;
 import com.pragma.powerup.infrastructure.out.jpa.mapper.IRestaurantEntityMapper;
 import com.pragma.powerup.infrastructure.out.jpa.repository.IRestaurantRepository;
+import java.util.Collections;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class RestaurantJpaAdapter implements IRestaurantPersistencePort {
@@ -21,5 +27,20 @@ public class RestaurantJpaAdapter implements IRestaurantPersistencePort {
             return false;
         }
         return true;
+    }
+
+    @Override
+    public Page<Restaurant> getRestaurants(Pageable pageable) {
+
+        Page<RestaurantEntity> restaurantEntities;
+
+        try{
+            restaurantEntities = restaurantRepository.findAll(pageable);
+        }
+        catch (Exception e){
+            return null;
+        }
+        
+        return restaurantEntityMapper.toRestaurantsPage(restaurantEntities);
     }
 }
