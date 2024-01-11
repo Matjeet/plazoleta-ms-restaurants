13bb379206e6cbd7c4651ae66771bf41f6a4b976
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-11 16:15:38 -0500

    feat: new IDishResponseMapper within application

---
+++ src/main/java/com/pragma/powerup/application/mapper/response/IDishResponseMapper.java
@@ -0,0 +1,19 @@
+package com.pragma.powerup.application.mapper.response;
+
+import com.pragma.powerup.application.dto.response.DishPageResponseDto;
+import com.pragma.powerup.domain.model.Category;
+import com.pragma.powerup.domain.model.Dish;
+import com.pragma.powerup.domain.model.Status;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        unmappedSourcePolicy = ReportingPolicy.IGNORE)
+public interface IDishResponseMapper {
+
+    @Mapping(source = "category.name", target = "category.name")
+    @Mapping(source = "status.name", target = "status.name")
+    DishPageResponseDto toDishPageDto(Dish dish, Category category, Status status);
+}
