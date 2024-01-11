f852a3317b18cfb63164321787436cac20d56321
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-11 16:32:30 -0500

    feat: new IStatusEntityMapper interface within infrastructure

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/StatusJpaAdapter.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/adapter/StatusJpaAdapter.java
@@ -3,6 +3,7 @@ package com.pragma.powerup.infrastructure.out.jpa.adapter;
 import com.pragma.powerup.domain.model.Status;
 import com.pragma.powerup.domain.spi.IStatusPersistencePort;
 import com.pragma.powerup.infrastructure.out.jpa.entity.StatusEntity;
+import com.pragma.powerup.infrastructure.out.jpa.mapper.IStatusEntityMapper;
 import com.pragma.powerup.infrastructure.out.jpa.repository.IStatusRepository;
 import lombok.RequiredArgsConstructor;
 
