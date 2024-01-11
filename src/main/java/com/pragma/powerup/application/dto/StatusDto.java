d66840dbe0d402b4801d58276b9efeabaeb99174
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-11 14:32:54 -0500

    feat: new StatusDto within application

--- src/main/java/com/pragma/powerup/application/dto/StatusDto.java
+++ src/main/java/com/pragma/powerup/application/dto/StatusDto.java
@@ -1,2 +1,11 @@
-package com.pragma.powerup.application.dto;public class StatusDto {
+package com.pragma.powerup.application.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StatusDto {
+
+    private String name;
 }
