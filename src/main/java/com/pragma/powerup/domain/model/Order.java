11d17106aca12116da9e6c33806a5cefaf1c2e1a
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-12 15:57:41 -0500

    feat: two new attributes into the Order class has been added

--- src/main/java/com/pragma/powerup/domain/model/Order.java
+++ src/main/java/com/pragma/powerup/domain/model/Order.java
@@ -1,16 +1,22 @@
 package com.pragma.powerup.domain.model;
 
+import java.util.List;
+
 public class Order {
 
     private int id;
+    private List<OrderDish> orderDishes;
     private int idRestaurant;
+    private int idStatus;
 
-    public Order() {
+    public Order(int id, List<OrderDish> orderDishes, int idRestaurant, int idStatus) {
+        this.id = id;
+        this.orderDishes = orderDishes;
+        this.idRestaurant = idRestaurant;
+        this.idStatus = idStatus;
     }
 
-    public Order(int id, int idRestaurant) {
-        this.id = id;
-        this.idRestaurant = idRestaurant;
+    public Order() {
     }
 
     public int getId() {
@@ -21,11 +27,27 @@ public class Order {
         this.id = id;
     }
 
+    public List<OrderDish> getOrderDishes() {
+        return orderDishes;
+    }
+
+    public void setOrderDishes(List<OrderDish> orderDishes) {
+        this.orderDishes = orderDishes;
+    }
+
     public int getIdRestaurant() {
         return idRestaurant;
     }
 
     public void setIdRestaurant(int idRestaurant) {
         this.idRestaurant = idRestaurant;
+    }
+
+    public int getIdStatus() {
+        return idStatus;
+    }
+
+    public void setIdStatus(int idStatus) {
+        this.idStatus = idStatus;
     }
 }
---
+++ src/main/java/com/pragma/powerup/domain/model/OrderDish.java
@@ -0,0 +1,2 @@
+package com.pragma.powerup.domain.model;public class OrderDish {
+}
