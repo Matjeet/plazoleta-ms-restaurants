3ea24e9c90c00a4f5295ce8e317678171233a25d
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-12 18:27:31 -0500

    feat: new OrderUseCase class within domain

---
+++ src/main/java/com/pragma/powerup/domain/usecase/OrderUseCase.java
@@ -0,0 +1,18 @@
+package com.pragma.powerup.domain.usecase;
+
+import com.pragma.powerup.domain.api.IOrderServicePort;
+import com.pragma.powerup.domain.model.Order;
+import com.pragma.powerup.domain.spi.IOrderPersistencePort;
+
+public class OrderUseCase implements IOrderServicePort {
+
+    private final IOrderPersistencePort orderPersistencePort;
+    public OrderUseCase(IOrderPersistencePort orderPersistencePort){
+        this.orderPersistencePort = orderPersistencePort;
+    }
+
+    @Override
+    public void saveOrder(Order order) {
+        orderPersistencePort.saveOrder(order);
+    }
+}
