2a6021fb42265d9c9f8f751cb93f48314c49befa
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-13 15:08:32 -0500

    feat: new IOrderRepository interface within infrastructure

---
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/repository/IOrderRepository.java
@@ -0,0 +1,7 @@
+package com.pragma.powerup.infrastructure.out.jpa.repository;
+
+import com.pragma.powerup.infrastructure.out.jpa.entity.OrderEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IOrderRepository extends JpaRepository<OrderEntity, Integer> {
+}
