a0b36a9260fe2bce46112dc09a7a8444b3e7a906
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-13 15:44:12 -0500

    feat: new OrderDishEntity class within infrastructure

--- src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/OrderDishEntity.java
+++ src/main/java/com/pragma/powerup/infrastructure/out/jpa/entity/OrderDishEntity.java
@@ -1,2 +1,30 @@
-package com.pragma.powerup.infrastructure.out.jpa.entity;public class OrderDishEntity {
+package com.pragma.powerup.infrastructure.out.jpa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "orderDish")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class OrderDishEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name = "dish_id", nullable = false)
+    private DishEntity dish;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private OrderEntity order;
 }
