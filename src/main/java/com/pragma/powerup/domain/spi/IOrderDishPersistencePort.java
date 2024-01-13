5020b0064c2e82882a09c606595761d2fe81621b
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-13 14:37:33 -0500

    fix: the requiered parameter for the saveOrderDish inside the IOrderDishPersistencePort has been changed

--- src/main/java/com/pragma/powerup/domain/spi/IOrderDishPersistencePort.java
+++ src/main/java/com/pragma/powerup/domain/spi/IOrderDishPersistencePort.java
@@ -2,7 +2,9 @@ package com.pragma.powerup.domain.spi;
 
 import com.pragma.powerup.domain.model.OrderDish;
 
+import java.util.List;
+
 public interface IOrderDishPersistencePort {
 
-    void saveOrderDish(OrderDish orderDish);
+    void saveOrderDish(List<OrderDish> orderDishes);
 }
