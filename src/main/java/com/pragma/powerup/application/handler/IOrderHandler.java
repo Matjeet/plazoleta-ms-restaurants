8dc14c51e04705fa1d509d93310d566f92832a8c
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-13 14:45:25 -0500

    feat: new OrderHandler class and interface within application

--- src/main/java/com/pragma/powerup/application/handler/IOrderHandler.java
+++ src/main/java/com/pragma/powerup/application/handler/IOrderHandler.java
@@ -1,2 +1,8 @@
-package com.pragma.powerup.application.handler;public interface IOrderHandler {
+package com.pragma.powerup.application.handler;
+
+import com.pragma.powerup.application.dto.request.RegisterOrderRequestDto;
+
+public interface IOrderHandler {
+
+    void saveOrder(RegisterOrderRequestDto registerOrderRequestDto);
 }
--- src/main/java/com/pragma/powerup/application/handler/impl/OrderDish.java
+++ src/main/java/com/pragma/powerup/application/handler/impl/OrderDish.java
@@ -1,2 +1,40 @@
-package com.pragma.powerup.application.handler.impl;public class OrderDish {
+package com.pragma.powerup.application.handler.impl;
+
+import com.pragma.powerup.application.dto.request.RegisterOrderRequestDto;
+import com.pragma.powerup.application.handler.IOrderHandler;
+import com.pragma.powerup.application.mapper.request.IOrderDishRequestMapper;
+import com.pragma.powerup.application.mapper.request.IOrderRequestMapper;
+import com.pragma.powerup.domain.api.IOrderDishServicePort;
+import com.pragma.powerup.domain.api.IOrderServicePort;
+import com.pragma.powerup.domain.api.IStatusServicePort;
+import com.pragma.powerup.domain.model.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class OrderDish implements IOrderHandler {
+
+    private final IOrderRequestMapper orderRequestMapper;
+    private final IOrderDishRequestMapper orderDishRequestMapper;
+    private final IOrderServicePort orderServicePort;
+    private final IOrderDishServicePort orderDishServicePort;
+    private final IStatusServicePort statusServicePort;
+
+    private static final String BACKORDER = "pendiente";
+    @Override
+    public void saveOrder(RegisterOrderRequestDto registerOrderRequestDto) {
+
+        int idStatus = statusServicePort.getStatusId(BACKORDER);
+
+        orderServicePort.saveOrder(orderRequestMapper.toOrder(registerOrderRequestDto, idStatus));
+
+        orderDishServicePort.saveOrderDish(
+                orderDishRequestMapper.toOrderDishList(
+                        registerOrderRequestDto.getOrderDishRequestDtos()
+                )
+        );
+    }
 }
