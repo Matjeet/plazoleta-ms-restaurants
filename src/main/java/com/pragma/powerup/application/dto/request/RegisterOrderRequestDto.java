97468644ce71198d22abbdb2a78319a5e080afa0
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-13 12:47:24 -0500

    feat: new RegisterOrderRequestDto within application

---
+++ src/main/java/com/pragma/powerup/application/dto/request/RegisterOrderRequestDto.java
@@ -0,0 +1,14 @@
+package com.pragma.powerup.application.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class RegisterOrderRequestDto {
+
+    private int idRestaurant;
+    private List<RegisterOrderDishRequestDto> orderDishRequestDtos;
+}
