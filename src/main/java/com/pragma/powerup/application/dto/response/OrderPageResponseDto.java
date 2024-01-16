226ac7775dbf7621675d3b5255ab7eb9ce1ecf5e
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-15 20:47:45 -0500

    feat: new dtos to response an OrderPageResponseDto

---
+++ src/main/java/com/pragma/powerup/application/dto/response/OrderPageResponseDto.java
@@ -0,0 +1,17 @@
+package com.pragma.powerup.application.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class OrderPageResponseDto {
+
+    private int id;
+    private int idClient;
+    private RestaurantsPageResponseDto restaurant;
+    private StatusOrderResponseDto status;
+    private List<OrderDishListResponseDto> orderDishes;
+}
---
+++ src/main/java/com/pragma/powerup/application/dto/response/StatusOrderResponseDto.java
@@ -0,0 +1,11 @@
+package com.pragma.powerup.application.dto.response;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StatusOrderResponseDto {
+
+    private String name;
+}
