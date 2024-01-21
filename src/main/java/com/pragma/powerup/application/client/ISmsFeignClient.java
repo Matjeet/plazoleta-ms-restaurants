3a7e4d787e47fa17077d2c8d085bcbbc4d610e64
Mateo Velásquez Morales <matius29002@gmail.com>
2024-01-21 13:39:28 -0500

    feat: new ISmsFeignClient interface within application

---
+++ src/main/java/com/pragma/powerup/application/client/ISmsFeignClient.java
@@ -0,0 +1,17 @@
+package com.pragma.powerup.application.client;
+
+import com.pragma.powerup.application.dto.request.SmsInfoRequestDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(
+        name = "microservice-sms",
+        url = "http://localhost:8083"
+)
+public interface ISmsFeignClient {
+
+    @PostMapping(value = "/sms/v1/send", consumes = MediaType.APPLICATION_JSON_VALUE)
+    int sendSms(@RequestBody SmsInfoRequestDto smsInfoRequestDto);
+}
